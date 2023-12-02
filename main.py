f02e51d1fcff641725ae83005c6c6199a341e6c8
Click41 <annakolb@brandeis.edu>
2023-12-02 16:05:48 -0500

    main machine learning file

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+# will have to add in more imports as we go along depending on what we use
+
+def preprocess_data(dataset):
+    
+    # where format our data to make sure everything matches for reading
+    
+    return dataset //
+
+def evaluation_model(model, x-test, y-test):
+        
+        # where we evaluate the model
+        ## predictions
+        ## accuracy
+        
+        return model 
+    
+def main():
+    
+    # loading dataset
+    dataset = pd.read_csv('data/MBTA_Reliability_Bus553.csv')
+    ### add in extra lines for more data
+    print(dataset.head())
+
+    # call preprocessing function
+    
+    # choose model but what model?
+    
+    #train model?
+    
+    # call evaluation function
+    
+if __name__ == '__main__':
+    main()
+
