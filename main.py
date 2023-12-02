c8acaa1ef9dc04c42f5b33913dc11b82f8730909
Click41 <annakolb@brandeis.edu>
2023-12-02 17:19:18 -0500

    sorry alex accidentally deleted ur stuff.

--- main.py
+++ main.py
@@ -61,35 +61,51 @@ def main():
     # ################ LOADING DATA SETS ################
     # reliability datasets - the col reliability_percentage = % of buses that are ON TIME (1 - % = late)
     # unreliable_percentage = % of times/buses that are late
-    # now contains columns for temp in Celsius/precip for that day 
     reliability_553 = pd.read_csv('./data/MBTA_Bus_Reliability_Bus553.csv')
     reliability = pd.read_csv('./data/MBTA_Bus_Reliability.csv')
     # alerts - gives dates buses are receiving alerts (particularly for delays) + reason
     alerts = pd.read_csv('data/BUS_Service_Alerts.csv') 
+    alerts = pd.read_csv('data/BUS_Service_Alerts.csv')
     # gives weather (avg temp + precipitation) for boston area
     boston_weather = pd.read_csv('data/boston_weather.csv')
     
+
     ## combines datasets into one dataframe
     combined_data = pd.concat([reliability_553, reliability, alerts, boston_weather], axis=1)
+    print(combined_data.columns)
     ## can takeout if we decide to use all 4 separately, just make sure to change function
 
     ### add in extra lines for more data
-    print(reliability.head())
+    print(combined_data.head())
+    # print(reliability.head())
 
     # ################ CALL PREPROCESSING FUNCTION ################
     processed_data = preprocess_data(combined_data)
-    
-    # calls testing function built into scikit-learn library
-    X_train, X_test, y_train, y_test = train_test_split(processed_data.drop('target_column', axis=1), 
-                                                    processed_data['target_column'], 
-                                                    test_size=0.2, random_state=42)
-    ## can change how we split data into sets/how many
-    
+
+    ## split data into training-testing sets
+    X = processed_data.drop('add_reliability_bool_col', axis=1)
+    y = processed_data['add_reliability_bool_col']
+
+    #split data into train-test 80-20
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
+
     # model from scikit-learn library
     model = GaussianNB() # Gaussian Naive Bayes
     
-    #train (like train a dog) model using our split data
+
+    #train (like train a dog) model that we choose (from scikit-learn library) specific to what we need
     model.fit(X_train, y_train) # where X_train is the training data and y_train is the target labels
+
+
+    #Make the prediction
+    y_pred_gnb = model.predict(X_test)
+
+    #Use confusion matrix and classification report to check the model's performance
+    conf_matrix = confusion_matrix(y_test, y_pred_gnb)
+
+    # Display the confusion matrix
+    print("Confusion Matrix:")
+    print(conf_matrix)
     
     # ################ CALL EVALUATION FUNCTION ################
     evaluation_model(model, X_test, y_test)
