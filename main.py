c9444c9a866a57571e5d499c3b2c904b41b6175a
Click41 <annakolb@brandeis.edu>
2023-12-05 18:28:48 -0500

    final changes (IT WORKS!!!)

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, precision_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def preprocess_data(dataset, test_size=0.2, random_state=42):
     
     # Assuming dataset has features (X) and target variable (y)
-    columns_to_drop = ['gtfs_route_id', 'route_category', 'mode_type', 'peak_offpeak_ind']
+    columns_to_drop = ['gtfs_route_id', 'mode_type', 'peak_offpeak_ind']
     X = dataset.drop(columns=columns_to_drop, axis=1)  # Features (input)
     y = dataset['unreliable_percentage']  # Target variable (output)
 
@@ -46,48 +45,37 @@ def preprocess_data(dataset, test_size=0.2, random_state=42):
     
     return X_train, X_test, y_train, y_test
 
-def evaluation_model(model, X_test, y_test): ## if have other parameters, add them in here
+def evaluation_model(model, X_test, y_test):
+    # Make predictions
+    y_pred = model.predict(X_test)
 
-    #Make the prediction
-    y_pred_gnb = model.predict(X_test)
-    
-    # classification report
-    class_report = classification_report(y_test, y_pred_gnb)
-    print("Classification Report:", class_report)
+    # Evaluate the model
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
 
-    #Use confusion matrix and classification report to check the model's performance
-    conf_matrix = confusion_matrix(y_test, y_pred_gnb)
-    print("Confusion Matrix:", conf_matrix)
-    
-    # accuracy score
-    accuracy = model.score(X_test, y_test)
-    print("Accuracy:", accuracy)
-    
-    # accuracy score 2
-    accuracy2 = accuracy_score(y_test, y_pred_gnb)
-    print("Accuracy2:", accuracy2)
-        
-    return model 
+    # # Print or return the metrics
+    # print(f"Mean Absolute Error: {mae}")
+    # print(f"Mean Squared Error: {mse}")
+    # print(f"Root Mean Squared Error: {rmse}")
+    # print(f"R-squared: {r2}")
+
+    return model
     
 def main():
     # ################ LOADING DATA SETS ################
     # reliability datasets - the col reliability_percentage = % of buses that are ON TIME (1 - % = late)
     # unreliable_percentage = % of times/buses that are late
-    
-    # now contains columns for temp in Celsius/precip for that day 
-    reliability_553 = pd.read_csv('./data/MBTA_Bus_Reliability_Bus553.csv')
-    
+        
     # alerts - gives dates buses are receiving alerts (particularly for delays) + reason
-    alerts = pd.read_csv('data/BUS_Service_Alerts.csv') 
+    alerts = pd.read_csv('./data/BUS_Service_Alerts.csv') 
     
     # gives weather (avg temp + precipitation) for boston area
-    boston_weather = pd.read_csv('data/boston_weather.csv')
+    boston_weather = pd.read_csv('./data/boston_weather.csv')
     
     # one that we use for most data accuracy
     reliability = pd.read_csv('./data/MBTA_Bus_Reliability.csv')
-
-    ### adds in extra lines for more data
-    # print(reliability.head())
 
     # ################ CALL PREPROCESSING FUNCTION ################
     # Preprocesses the entire dataset
@@ -95,42 +83,13 @@ def main():
     
     # ################ MODEL TRAINING ################
     # model from scikit-learn library
-    model = GaussianNB() # Gaussian Naive Bayes
-
-    ################ TROUBLE SHOOTING ################
-    # Convert NumPy array to Pandas DataFrame
-    # X_train_df = pd.DataFrame(X_train)
-    # y_train_df = pd.DataFrame(y_train)
-    
-    # print(y_train)
-    
-    # print("Data type of X_train:", X_train.dtype)
-    # print("Data type of y_train:", y_train.dtype)
-    ## they are both float64
-    
-    # Check for NaN or infinity in X_train
-    # print("NaN in X_train:", y_train_df.isna().any().any())
-    # print("Infinity in X_train:", y_train_df.isna().any().any())
-    
-    # to find which column has NaN
-    # print("NaN in each column of y_train:")
-    # for col in y_train_df.columns:
-    #     print(f"{col}: {y_train_df[col].isna().any()}")
-    
-    # print("NaN in each column of X_train:")
-    # for col in X_train_df.columns:
-    #     print(f"{col}: {X_train_df[col].isna().any()}")
-
-    # # Check for NaN or infinity in y_train
-    # print("NaN in y_train:", np.isnan(y_train_df).any())
-    # print("Infinity in y_train:", np.isinf(y_train_df).any())
-    ##################################################
+    model = LinearRegression() # Linear Regression model
 
     #train (like train a dog) model that we choose (from scikit-learn library) specific to what we need
     model.fit(X_train, y_train) # where X_train is the training data and y_train is the target labels
     
     # ################ CALL EVALUATION FUNCTION ################
-    # evaluation_model(model, X_test, y_test)
+    evaluation_model(model, X_test, y_test)
     
 if __name__ == '__main__':
     main()
